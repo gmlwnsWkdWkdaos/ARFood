e1617912a9f582540b959db7f1cf5aece530180f
pjj9606 <55330623+pjj9606@users.noreply.github.com>
2021-05-31 07:51:29 +0900

    what
    안바꿨는데 왜 커밋할게있지;

--- FoodAR/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++ FoodAR/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
@@ -1,4 +1,4 @@
 #include "il2cpp-config.h"
-#include "C:\Users\JUNE\Desktop\ARFood\FoodAR\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "C:\Users\JUNE\Desktop\ARFood\FoodAR\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
-#include "C:\Users\JUNE\Desktop\ARFood\FoodAR\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
+#include "C:\Users\2015125080\Documents\GitHub\ARFood\FoodAR\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
+#include "C:\Users\2015125080\Documents\GitHub\ARFood\FoodAR\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
+#include "C:\Users\2015125080\Documents\GitHub\ARFood\FoodAR\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
