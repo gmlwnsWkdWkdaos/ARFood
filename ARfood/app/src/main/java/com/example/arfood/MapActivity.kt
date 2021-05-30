22ebd22b489964648e0096e7a637b2f6f4a3c91c
ZZergling <joyjune95@gmail.com>
2021-05-31 07:12:54 +0900

    지도....

---
+++ ARfood/app/src/main/java/com/example/arfood/MapActivity.kt
@@ -0,0 +1,17 @@
+package com.example.arfood
+
+import android.R.layout
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
+import net.daum.mf.map.api.MapView;
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
--- ARfood/app/src/main/java/com/example/arfood/SearchActivity.kt
+++ ARfood/app/src/main/java/com/example/arfood/SearchActivity.kt
@@ -2,11 +2,22 @@ package com.example.arfood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import kotlinx.android.synthetic.main.activity_map.*
+import net.daum.mf.map.api.MapView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_store)
+        setContentView(R.layout.activity_map)
+
+        val mapView = MapView(this)
+
+        val mapViewContainer = map_view as ViewGroup
+
+        mapViewContainer.addView(mapView)
     }
 
 
